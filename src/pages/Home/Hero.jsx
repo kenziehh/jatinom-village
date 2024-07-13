628896bb3abd4891f61189c017181e94461514f0
kenziehh <ghaisani.nurani@gmail.com>
2024-07-13 10:59:29 +0700

    style: adjust style and update text at hero

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -3,33 +3,33 @@ import React from "react";
 const Hero = () => {
   return (
     <section className="bg-dark-green min-h-screen pb-10 pt-[10vh] md:pt-[20vh] md:pb-20">
-      <div className="container flex flex-col-reverse gap-12 md:flex-row md:justify-between">
+      <div className="container flex flex-col-reverse gap-12 md:flex-row md:justify-between items-center">
         <div className="flex flex-col items-center md:items-start gap-8 md:gap-24">
           <div className="flex flex-col gap-4 md:gap-8 text-center md:text-start">
-            <h1 className="text-4xl md:text-6xl text-white font-semibold">
+            <h1 className="text-4xl md:text-5xl xl:text-6xl text-white font-semibold">
               Desa Jatinom
             </h1>
             <p className="max-w-md text-white">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-              cupiditate ut quos, aliquid veniam voluptates explicabo, sunt
-              accusamus possimus pariatur fugiat ullam maiores voluptatem!
-              Facilis ratione unde asperiores similique natus!
+              Terletak di Kecamatan Kanigoro, Kabupaten Blitar, Jawa Timur, Desa
+              Jatinom menyimpan pesona dan potensi yang menanti untuk
+              dijelajahi. Desa ini menawarkan perpaduan harmonis antara kekayaan
+              alam, budaya, dan keramahan masyarakatnya, menjadikannya destinasi
+              wisata yang menarik bagi para pelancong.
             </p>
           </div>
 
-          <div className="bg-white shadow-md rounded-2xl py-3 px-6 flex items-center gap-2">
-            <img
-              src="/assets/icons/map.png"
-              alt=""
-              className="w-8 h-8"
-            />
+          <div className="bg-white shadow-md rounded-2xl py-3 px-6 flex items-center gap-2 justify-center">
+            <img src="/assets/icons/map.png" alt="" className="w-8 h-8" />
             <span className="text-gray-400">Kanigoro, Kabupaten Blitar</span>
           </div>
         </div>
         <div className="">
-            <img src="https://placehold.co/600x400" alt="" className="rounded-2xl" />
+          <img
+            src="https://placehold.co/600x400"
+            alt=""
+            className="rounded-2xl"
+          />
         </div>
-
       </div>
     </section>
   );
